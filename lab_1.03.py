b824c343cd2a0f49dfef405211a76dae9095b541
bewillis25 <97301460+bewillis25@users.noreply.github.com>
2022-01-18 09:34:24 -0600

    Add files via upload

---
+++ lab_1.03.py
@@ -0,0 +1,35 @@
+'''
+Lab 1.03 - Printing & Variables
+
+Part 1 - Printing Practice
+Practice typing out some statements in the editor part of the IDE, then hit "Run" at the top of the screen:
+**Remember you must exit() python interpreter before running a script!!!
+
+    Expression                                      Expected Output                               Did anything unexpected happen?
+a)  print("1")                                               1
+b)  print(1)                                                 1
+c)  print(1 + 2)                                             3
+d)  print("1" + "2")                                         12
+e)  print("this" + " " + "is" + " " +                        
+    "a" + " " +
+    "sentence" + ".")
+
+Part 2 - Variables Practice
+1. In your Console
+Type and run the following:
+    animal = "dogs"
+    print(animal + " are really cool.")
+In your Notebook
+Respond to the following:
+    1. What happens?
+    2. How would you make the program print out "cats are really cool" instead?
+
+2. In your Console
+Type and run the following code:
+print(dogs + " are cool.")
+In your Notebook
+Respond to the following questions:
+    1. What output does this produce?
+    2. Why does this happen?
+'''
+print(1)

1da03e999046e2608b33871f800601f2fa1979b9
bewillis25 <bewillis25@students.bmchs.org>
2022-01-21 09:47:23 -0600

    Mad-Libs Project

--- Unit1_Project_Mad_Libs.py
+++ Unit1_Project_Mad_Libs.py
@@ -14,5 +14,5 @@ verb1 = input("Enter a past tense vern someone can do to someone else, i.e. punc
 animal2 = input("Enter another animal: ")
 adj1 = input("Enter an adjective: ")
 noun1 = input("Enter your favorite band: ")
-print(f"One day in {place1}, a man named {name1} was eating lunch when all of a sudden an agressive {animal1} named {animalname1} flew by and stole his food. The man {adverb1} got into his {vehicle1} and chased after {animalname1}. The man was catching up to {animalname1} when a {color1} {tool1} hit him on the head. He was just getting up when a {adj1} {animal2} in a {size1} {clothes1} {verb1} {animalname1}. The man went and thanked the {animal2} and went off to see {noun1}.The End.")
+print(f"One day in {place1}, a man named {name1} was eating lunch when all of a sudden an agressive {animal1} named {animalname1} flew by and stole his food. The man {adverb1} got into his {vehicle1} and chased after {animalname1}. The man was catching up to {animalname1} when a {color1} {tool1} hit him on the head. He was just getting up when a {adj1} {animal2} in a {size1} {clothes1} {verb1} {animalname1}. The man went and thanked the {animal2} and went off to see {noun1}. The End.")
 
